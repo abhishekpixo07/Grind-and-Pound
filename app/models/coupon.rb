76649ef0cada693262c3b10ed85fe726aefd0ea4
abhishekpixo07 <abhishek.k@pixoatic.com>
2024-03-01 11:51:00 +0530

    order , payment api with razorpay

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,4 +3,10 @@ class Coupon < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["active", "applicability", "code", "created_at", "description", "discount_percentage", "expiry_date", "id", "max_discount_amount", "minimum_purchase_amount", "no_of_uses", "stackable", "unique_per_user", "updated_at", "usage_restrictions", "user_limit"]
   end
+
+  def applicable?(total_price)
+    return true if minimum_purchase_amount.nil? || total_price >= minimum_purchase_amount
+
+    false
+  end
 end
