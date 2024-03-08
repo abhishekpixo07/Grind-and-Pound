185d01a5843e16d7f98df73c14b5bb0fe7294f05
abhishekpixo07 <abhishek.k@pixoatic.com>
2024-03-08 12:02:08 +0530

    order model and admin changes

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -24,21 +24,41 @@ ActiveAdmin.register Order do
       row :tracking_number
       # Add more attributes as needed
 
-      # Display Payment Information
-      if order.payment.present?
-        row :payment_status do
-          order.payment.payment_status
+      panel "Payment Information" do
+        attributes_table_for resource.payment do
+          row :payment_method do
+            order.payment_method
+          end          
+          row :payment_status
+          row :payment_amount do
+            number_to_currency(order.payment.payment_amount, unit: '₹') # Adjust currency as needed
+          end
+          row :created_at
+          row :updated_at
         end
-        row :payment_amount do
-          number_to_currency(order.payment.payment_amount, unit: '₹') # Adjust currency as needed
+      end if resource.payment.present?
+
+      panel "Shipping Address" do
+        attributes_table_for resource.shipping_address do
+          row :address_line_1
+          row :address_line_2
+          row :city
+          row :state
+          row :postal_code
+          row :country
         end
-        row :created_at do
-          order.payment.created_at
-        end
-        row :updated_at do
-          order.payment.updated_at
+      end if resource.shipping_address.present?
+
+      panel "User Details" do
+        attributes_table_for resource.user do
+          row :email
+          row :first_name
+          row :last_name
+          row :phone_number
+          # Add more user attributes as needed
         end
       end
+
     end
   end
 end
