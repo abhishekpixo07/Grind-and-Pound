56e2d3e5a9265b3ba70570d6f8ce3aa4062879af
abhishekpixo07 <abhishek.k@pixoatic.com>
2024-03-11 10:28:19 +0530

    order model and order items model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "order_id", "product_id", "quantity", "total_price", "unit_price", "updated_at"]
+  end
+
 end
